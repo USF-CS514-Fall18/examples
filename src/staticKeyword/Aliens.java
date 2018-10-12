f6da775fdad9b6022c6e4b61aa7c40d4836ba019
Olga Karpenko <okarpenko@usfca.edu>
2018-10-11 21:28:48 -0700

    Updated Martian/ Aliens example in staticKeyword package.

--- src/staticKeyword/Aliens.java
+++ src/staticKeyword/Aliens.java
@@ -1,6 +1,12 @@
 package staticKeyword;
 
-/** The driver class for class Martian */
+/** The driver class for class Martian.
+ *  Shows:
+ *  1) How behavior of dealWithEnemy method changes depending on how
+ *     many Martian objects exist.
+ *  2) Shows that any Martian can change the slogan, and all other martians
+ *     will see that change (since slogan is a static variable)
+ */
 public class Aliens {
     public static void main(String[] args) {
         Martian m1 = new Martian("beebeesfqpjfq");
@@ -9,10 +15,26 @@ public class Aliens {
         Martian m2 = new Martian("tatadam-tatadam-tatadam-tatadam");
         m2.dealWithEnemy();
         Martian m3 = new Martian("yammi,yammi,yammi");
+        System.out.println(System.lineSeparator() + "Total number of martians: " + Martian.getNumMartians());
+
         m1.dealWithEnemy();
         m2.dealWithEnemy();
         m3.dealWithEnemy();
-        System.out.println(System.lineSeparator() + "Total number of martians: " + Martian.getNumMartians());
+
+        System.out.println("m1 changes the slogan; everybody sees this change: ");
+        m1.changeSlogan("Mars rocks!");
+        System.out.println(Martian.getSlogan());
+        System.out.println(m1.getSlogan());
+        System.out.println(m2.getSlogan());
+        System.out.println(m3.getSlogan());
+
+        System.out.println();
+        System.out.println("m3 changes the slogan; everybody sees this change: ");
+        m3.changeSlogan("We love Mars!");
+        System.out.println(Martian.getSlogan());
+        System.out.println(m3.getSlogan());
+        System.out.println(m1.getSlogan());
+        System.out.println(m2.getSlogan());
 
     }
 }
--- src/staticKeyword/Martian.java
+++ src/staticKeyword/Martian.java
@@ -3,17 +3,32 @@ package staticKeyword;
 /** The class demonstrates the use of a static variable */
 public class Martian {
     private String name;
-    private static int numMartians = 0;
+    private static int numMartians = 0; // total number of Martian objects created
 
-    public Martian(String n) {
-        name = n;
+    private static String slogan; // slogan of Martians.
+
+    /**
+     * Constructor for class Martian
+     * @param name name of this alien
+     */
+    public Martian(String name) {
+        this.name = name;
         numMartians++;
     }
 
+    /**
+     * Getter for the name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * How a Martian deals with an enemy:
+     * if there are less than 3 martians, hide
+     * if there are 3 or more martians total, then fight
+     */
     public void dealWithEnemy() {
         System.out.print("My name is " + name + ". ");
         if (numMartians < 3) {
@@ -24,7 +39,33 @@ public class Martian {
         }
     }
 
+    /**
+     * Changes the slogan of Martians. Static method.
+     * @param newSlogan new slogan
+     */
+    public static void changeSlogan(String newSlogan) {
+        slogan = newSlogan;
+    }
+
+    public void setSlogan(String sl) {
+        slogan = sl;
+    }
+
+    /**
+     * Returns the slogan of martians - static method
+     * @return slogan
+     */
+    public static String getSlogan() {
+        return slogan;
+
+    }
+
+    /**
+     * Returns the total number of Martians
+     * @return number of Martians
+     */
     public static int getNumMartians() {
+
         return numMartians;
     }
 
