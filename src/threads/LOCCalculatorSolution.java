a28bc31a44a3f9c7591576dbffb2972911ff1154
Olga Karpenko <okarpenko@usfca.edu>
2018-10-22 12:35:49 -0700

    LOCCalculator Solution posted.

---
+++ src/threads/LOCCalculatorSolution.java
@@ -0,0 +1,67 @@
+package threads;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/** Two threads will count the number of lines of code in two different files
+ *  at the same time  (one thread per file).
+ */
+public class LOCCalculatorSolution {
+
+    public static class LOCWorker extends Thread {
+        private String filename;
+        private int count = 0;
+
+        LOCWorker(String filename) {
+            this.filename = filename;
+        }
+
+        @Override
+        public void run() {
+            count = 0;
+            try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+                String line;
+                while ((line  = br.readLine()) != null) {
+                    // note: this is not a good way to count lines of code;
+                    // For instance, it won't count a line if it has a *. But the
+                    // main point of this exercise is multithreading, not good LOC
+                    if ((!line.trim().isEmpty()) && !line.trim().startsWith("/") && (!line.trim().contains("*"))) {
+                        count++;
+                    }
+                }
+            }
+            catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        /**
+         * Returns count
+         * @return
+         */
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Paths.get(".").toAbsolutePath().toString());
+        Thread t1 = new LOCWorker("./src/threads/PrintTaskExample.java");
+        t1.start();
+        Thread t2 = new LOCWorker("./src/threads/PrintTaskExampleWithJoin.java");
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+            System.out.println(((LOCWorker) t1).getCount());
+            System.out.println(((LOCWorker) t2).getCount());
+        }
+        catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+
+    }
+}
